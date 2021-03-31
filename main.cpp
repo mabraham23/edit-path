130161df0fa258531493cc1db6401188450879e1
Mark Abraham <markabraham014@gmail.com>
2021-03-30 20:33:23 -0600

    fixed output issues

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "fstream"
 #include <string>
 #include <vector>
+#include <sstream>
 
 int diff( std::string word_1, std::string word_2, int i, int j ) {
     if ( word_1[i] == word_2[j]) {
@@ -33,20 +34,95 @@ int edit_distance( std::string word_1, std::string word_2 ) {
         }
     }
 
-    std::cout << "\n";
-    for( int i=0; i<word_1.length()+1; i++ ) {
-        for( int j=0; j<word_2.length()+1; j++ ) {
-            if ( (table[i][j] > 9 )) {
-                std::cout << " " << table[i][j];
-            }
-            else {
-                std::cout << " " << table[i][j] << " ";
-            }
-        }
-        std::cout << "\n";
-    }
+    // std::cout << "\n";
+    // for( int i=0; i<word_1.length()+1; i++ ) {
+    //     for( int j=0; j<word_2.length()+1; j++ ) {
+    //         if ( (table[i][j] > 9 )) {
+    //             std::cout << " " << table[i][j];
+    //         }
+    //         else {
+    //             std::cout << " " << table[i][j] << " ";
+    //         }
+    //     }
+    //     std::cout << "\n";
+    // }
 
     return table[word_1.length()][word_2.length()];
+}
+
+void read_files( std::vector<std::string> files ) {
+    std::ofstream r("output/edit-distance-cost.csv");
+	std::ofstream p("output/edit-distance-timing.csv");
+
+	double size_avg_time = 0;
+	int size_avg_reps = 0;
+	int prev_graph_size = 4;
+
+	for ( auto file: files ) {
+		double avgTime = 0;
+		double reps = 0;
+		int totalCost = 0;
+
+		int lastDash = file.rfind('-');
+		int firstDash = file.rfind('-', lastDash - 1);
+		int fileSize = atoi(file.substr(firstDash + 1, lastDash - firstDash - 1).c_str());
+
+		std::ifstream f;
+		f.open(file);
+		if (!f.good()) {
+			std::cerr << "Error reading file: " << file << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		std::string line;
+
+		// while (getline(f, line)) {
+		// 	if (line.at(0) == '#')
+		// 		continue;
+		// 	else
+		// 		break;
+		// }
+
+        // int testreps = 1;
+		// if ( fileSize <= 100000 ) {
+		// 	testreps = 1;
+		// }
+		// if ( fileSize <= 10000 ) {
+		// 	testreps = 1;
+		// }
+		// if ( fileSize <= 1000 ) {
+		// 	testreps = 1;
+		// }
+
+		while (getline(f, line)) {
+            std::string word_1, word_2;
+			std::istringstream ss(line);
+            ss >> word_1 >> word_2;
+            double t1, t2;
+            t1 = clock();
+            totalCost += edit_distance(word_1, word_2);
+            t2 = (clock() - t1 ) / CLOCKS_PER_SEC;
+            avgTime += t2;
+            // }
+		}
+		f.close();
+
+		if ( prev_graph_size == fileSize ) {
+			size_avg_time += avgTime;
+			size_avg_reps += 1;
+			if ( file == "words/words-1000-4.txt" ) {
+				r << fileSize << ", " << size_avg_time / size_avg_reps << std::endl;
+			}
+		}
+		else {
+			r << prev_graph_size << ", " << size_avg_time / size_avg_reps << std::endl;
+			prev_graph_size = fileSize;
+			size_avg_time = avgTime;
+			size_avg_reps = 1;
+		}
+		p << file.substr(6, file.length()) << ", " << avgTime << ", " << totalCost << std::endl;
+	}
+	r.close();
+	p.close();
 }
 
 bool compareFileSize(std::string f1, std::string f2) {
@@ -59,96 +135,6 @@ bool compareFileSize(std::string f1, std::string f2) {
     int size2 = atoi(f2.substr(beginIdx + 1, endIdx - beginIdx - 1).c_str());
 
     return size1 < size2;
-}
-
-void read_files( std::vector<std::string> files ) {
-    std::ofstream t("output/prim-timing.csv");
-	std::ofstream p("output/prim-timing-graphs.csv");
-
-	double size_avg_time =0;
-	int size_avg_reps = 0;
-	int prev_graph_size = 10;
-
-	for ( auto file: files ) {
-		double avgTime = 0;
-		double reps = 0;
-		int totalCost = 0;
-
-		int lastDash = file.rfind('-');
-		int firstDash = file.rfind('-', lastDash - 1);
-		int graphSize = atoi(file.substr(firstDash + 1, lastDash - firstDash - 1).c_str());
-
-		std::ifstream f;
-		f.open(file);
-		if (!f.good()) {
-			std::cerr << "Error reading file: " << file << std::endl;
-			exit(EXIT_FAILURE);
-		}
-		std::string line;
-
-		// First line is the number of Vertices
-		while (getline(f, line)) {
-			if (line.at(0) == '#')
-				continue;
-			else
-				break;
-		}
-
-		int V = std::stoi(line);
-		struct Graph* graph = createGraph(V); 
-		while (getline(f, line)) {
-            std::string word_1, word_2;
-			int v1, v2, weight;
-			// std::istringstream ss(line);
-            ss >> word_1 >> word_2;
-			ss >> v1 >> v2 >> weight;
-			v1 --;
-			v2 --;
-			addEdge(graph, v1, v2, weight); 
-		}
-		f.close();
-
-		int testreps = 1;
-		if ( graphSize <= 100000 ) {
-			testreps = 1;
-		}
-		if ( graphSize <= 10000 ) {
-			testreps = 1;
-		}
-		if ( graphSize <= 1000 ) {
-			testreps = 1;
-		}
-		for ( size_t i = 0; i < testreps; i++ ) {
-			reps ++;
-			double t1, t2;
-			t1 = clock();
-			totalCost = PrimMST(graph); 
-			t2 = (clock() - t1 ) / CLOCKS_PER_SEC;
-			avgTime += t2;
-		}
-
-		double graphTime = 0;
-		graphTime = avgTime / reps;
-
-		if ( prev_graph_size == graphSize ) {
-			size_avg_time += graphTime;
-			size_avg_reps += 1;
-			if ( file == "graphs/graph-800000-4.txt" ) {
-				std::cout << graphSize << ", " << size_avg_time / size_avg_reps << std::endl;
-				t << graphSize << ", " << size_avg_time / size_avg_reps << "\n";
-			}
-		}
-		else {
-			std::cout << prev_graph_size << ", " << size_avg_time / size_avg_reps << std::endl;
-			t << prev_graph_size << ", " << size_avg_time / size_avg_reps << "\n";
-			prev_graph_size = graphSize;
-			size_avg_time = graphTime;
-			size_avg_reps = 1;
-		}
-		p << file << ", " << avgTime / reps << ", " << totalCost << std::endl;
-	}
-	t.close();
-	p.close();
 }
 
 int main( int argc, const char* argv[] ) {
@@ -164,11 +150,11 @@ int main( int argc, const char* argv[] ) {
     read_files(files);
 
     // read in the words to be processed
-    std::string word_1;
-    std::string word_2;
-    word_1 = argv[1];
-    word_2 = argv[2];
-    int cost = edit_distance(word_1, word_2);
-    std::cout << "\nMinimum edit cost is " << cost << std::endl << "\n";
+    // std::string word_1;
+    // std::string word_2;
+    // word_1 = argv[1];
+    // word_2 = argv[2];
+    // int cost = edit_distance(word_1, word_2);
+    // std::cout << "\nMinimum edit cost is " << cost << std::endl << "\n";
     return 0;
 }
